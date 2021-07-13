c680db173869234798254139af31843ae839bb77
IT21119644 <87364036+IT21119644@users.noreply.github.com>
2021-07-13 09:49:08 +0000

    wrote the program (1st commit)

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,15 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1, mark2;
+  printf("Enter mark1: ");
+  scanf("%d", &mark1);
+
+  printf("Enter mark2: ");
+  scanf("%d", &mark2);
+
+  float avg = (mark1 + mark2) / 2.0;
+  printf("Average: %.2f", avg);
   return 0;
 }
 
